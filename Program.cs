c1eac29538f5f545178ca4ed621464a93ac44387
WW <abc123@gmail.com>
2024-11-19 20:24:15 +0200

    task3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace App19._11
+﻿using App19._11.Entities;
+
+namespace App19._11
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введіть елементи масиву через пробіл:");
+            string[] input = Console.ReadLine().Split(' ');
+            int[] array = Array.ConvertAll(input, int.Parse);
         }
     }
 }
