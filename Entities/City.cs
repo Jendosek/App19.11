8ab8086e7bd670d95f4c0a724b54edc7d827ea11
WW <abc123@gmail.com>
2024-11-19 20:46:52 +0200

    task4

--- Entities/City.cs
+++ Entities/City.cs
@@ -1,7 +1,17 @@
-﻿namespace App19._11.Entities
+﻿using System.Text.RegularExpressions;
+
+namespace App19._11.Entities
 {
     internal class City
     {
-
+        public string cityName {  get; set; }
+        public string countryName { get; set; }
+        public int population { get; set; }
+        public string phoneCode { get; set; }
+        public string districts { get; set; }
+        public string Print()
+        {
+            return $"Місто: {cityName}\nКраїна: {countryName}\nНаселення: {population}\nКод телефона: {phoneCode}\n Район: {districts}";
+        }
     }
 }
