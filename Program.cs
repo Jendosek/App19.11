8b7afcb143820fa316326d878765ad6b7a58b830
WW <abc123@gmail.com>
2024-11-19 19:44:19 +0200

    task1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace App19._11
+﻿using System.Text;
+
+namespace App19._11
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+
+            Console.Write("Enter first num of range: ");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter first num of range: ");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+            long dobutok = 1;
+
+            if (num2 < num1)
+            {
+                (num1, num2) = (num2, num1);
+            }
+
+            for (int i = num1; i <= num2; i++)
+            {
+                dobutok *= i;
+            }
+
+            Console.WriteLine($"Добуток чисел від {num1} до {num2} дорівнює: {dobutok}");
         }
     }
 }
