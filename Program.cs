58d6b79e111c19796b615c72e2cb7c908cdc4bff
WW <abc123@gmail.com>
2024-11-19 20:08:27 +0200

    task2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
-﻿namespace App19._11
+﻿using System;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace App19._11
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static bool FibonacciEquals(int number)
         {
-            Console.WriteLine("Hello, World!");
+            if (number < 0)
+                return false;
+
+            int a = 0;
+            int b = 1;
+
+            while (a <= number)
+            {
+                if (a == number)
+                    return true;
+
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+
+            return false;
+        }
+
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.Write("Enter num: ");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter range: ");
+            var num2 = Convert.ToInt32(Console.ReadLine());
+
+            bool isFibonacci = FibonacciEquals(num1);
+
+            Console.WriteLine(isFibonacci);
+                
         }
     }
 }
