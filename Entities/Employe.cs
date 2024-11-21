cbad743122cc08f448fef44de3ea1e282726071b
Jendosek <bala01081977@gmail.com>
2024-11-21 23:24:54 +0200

    task5

--- Entities/Employe.cs
+++ Entities/Employe.cs
@@ -2,6 +2,27 @@
 {
     internal class Employe
     {
+        public string cityName { get; set; }
+        public string countryName { get; set; }
+        public int population { get; set; }
+        public string phoneCode { get; set; }
+        public string districts { get; set; }
+
+        public City()
+        {
+            cityName = "";
+            countryName = "";
+            population = 0;
+            phoneCode = "";
+            districts = "";
+        }
+
+        public string Print()
+        {
+            return $"Місто: {cityName}\nКраїна: {countryName}\nНаселення: {population}\nКод телефона: {phoneCode}\n Район: {districts}";
+        }
+
 
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace App19._11
+﻿using System.Text;
+
+namespace App19._11
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            
         }
     }
 }
