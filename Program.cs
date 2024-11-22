6875afe1a99057af13f94dc8a0102c48c57d1f04
Jendosek <bala01081977@gmail.com>
2024-11-23 01:13:12 +0200

    task1_hm

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace App19._11
+﻿using System.Text;
+
+namespace App19._11
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введіть довжину сторони квадрата:");
+            int line = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть символ для виведення:");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Виведення квадрата:");
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < line; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
